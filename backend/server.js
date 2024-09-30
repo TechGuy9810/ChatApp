7fbcad1c3cefcc728576d6ee9dda5078a5898c8f
TechGuy9810 <140731793+TechGuy9810@users.noreply.github.com>
2024-09-30 12:13:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname,"/frontend/dist")))
 app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
 })
-server.listen(PORT,()=>{
+server.listen(PORT,"0.0.0.0"()=>{
     connectMongo();
     console.log(`Server is running on port ${PORT}`);
 
